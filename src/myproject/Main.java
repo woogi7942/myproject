cbd1cc2e9eecdfc75b4a9068d2086392311320e8
woogi7942 <woogi7942@naver.com>
2023-12-01 15:00:11 +0900

    v3.0

--- src/myproject/Main.java
+++ src/myproject/Main.java
@@ -3,7 +3,8 @@ package myproject;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("java");
 
 	}
 
